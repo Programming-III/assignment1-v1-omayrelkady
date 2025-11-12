7f63fb0c545004af4f4919183e1e790af6ba11a0
omayrelkady <omayrahmed06@gmail.com>
2025-11-12 15:20:45 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -4,7 +4,28 @@
 #include <string>
 using namespace std;
 
-//define visitor class here
+//define visitor cclass Visitor {
+private:
+    string visitorName;
+    int ticketsBought;
+
+public:
+    Visitor() : visitorName(""), ticketsBought(0) {}
+    Visitor(const string& name, int tickets) : visitorName(name), ticketsBought(tickets) {}
+    ~Visitor() {}
+
+    string getVisitorName() const { return visitorName; }
+    void setVisitorName(const string& n) { visitorName = n; }
+
+    int getTicketsBought() const { return ticketsBought; }
+    void setTicketsBought(int t) { ticketsBought = t; }
+
+    void displayInfo() const {
+        cout << "Visitor Info:\n";
+        cout << "Name: " << visitorName << "\n";
+        cout << "Tickets Bought: " << ticketsBought << "\n";
+    }
+};lass here
 
 
 

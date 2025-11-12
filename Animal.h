c1472bbb5df33c73f791cb4743e7ff7692e62f12
omayrelkady <omayrahmed06@gmail.com>
2025-11-12 15:27:23 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -4,7 +4,7 @@
 #include <string>
 using namespace std;
 
-//define animal class here 
+ 
 class Animal {
 private:
     string name;
@@ -12,13 +12,13 @@ private:
     bool isHungry;
 
 public:
-    // Constructors / Destructor
+    
     Animal() : name(""), age(0), isHungry(false) {}
     Animal(const string& name, int age, bool isHungry)
         : name(name), age(age), isHungry(isHungry) {}
     virtual ~Animal() {}
 
-    // Getters / Setters
+    /
     string getName() const { return name; }
     void setName(const string& n) { name = n; }
 
@@ -28,14 +28,14 @@ public:
     bool getIsHungry() const { return isHungry; }
     void setIsHungry(bool h) { isHungry = h; }
 
-    // Methods
+   
     virtual void display() const {
         cout << name << " (Age: " << age << ", "
              << (isHungry ? "Hungry" : "Not Hungry") << ")";
     }
 
     virtual void feed() {
-        // Simple behavior: if hungry, feeding makes it not hungry; otherwise do nothing.
+        /
         if (isHungry) isHungry = false;
     }
 

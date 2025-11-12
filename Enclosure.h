8b586a910a43017801a31d78e2b5010f062f72f0
omayrelkady <omayrahmed06@gmail.com>
2025-11-12 15:18:33 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -4,6 +4,26 @@
 #include "Animal.h"
 
 //define enclosure class here 
+class Reptile : public Animal {
+private:
+    bool isVenomous;
+
+public:
+    Reptile() : Animal(), isVenomous(false) {}
+    Reptile(const string& name, int age, bool isHungry, bool isVenomous)
+        : Animal(name, age, isHungry), isVenomous(isVenomous) {}
+    ~Reptile() override {}
+
+    bool getIsVenomous() const { return isVenomous; }
+    void setIsVenomous(bool v) { isVenomous = v; }
+
+    void display() const override {
+        // Must include "Venomous" before hunger state, per sample output
+        cout << getName() << " (Age: " << getAge() << ", "
+             << (isVenomous ? "Venomous, " : "")
+             << (getIsHungry() ? "Hungry" : "Not Hungry") << ")";
+    }
+};
 
 
 

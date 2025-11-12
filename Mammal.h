0323cfb92a60d25d4664d9f479666ed26d75fbe6
omayrelkady <omayrahmed06@gmail.com>
2025-11-12 15:29:06 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -3,7 +3,7 @@
 
 #include "Animal.h"
 
-//define mammal class here
+
 lass Mammal : public Animal {
 private:
     string furColor;
@@ -18,7 +18,7 @@ public:
     void setFurColor(const string& c) { furColor = c; }
 
     void display() const override {
-        // Output matches required format (no extra fields shown in sample)
+       
         Animal::display();
     }
 };

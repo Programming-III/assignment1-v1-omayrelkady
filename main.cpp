0a46c3c58cb2c2754ff64a7769e82ade64b743d0
omayrelkady <omayrahmed06@gmail.com>
2025-11-12 15:22:53 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,9 +8,31 @@
 using namespace std;
 
 
-// ============== MAIN FUNCTION ==============
+
 int main() {
     
+    Enclosure* e1 = new Enclosure(3);
+
     
+    Animal* a1 = new Mammal("Lion", 5, true, "Golden");       
+    Animal* a2 = new Bird("Parrot", 2, false, 0.35f);         
+    Animal* a3 = new Reptile("Snake", 3, true, true);         
+    e1->addAnimal(a1);
+    e1->addAnimal(a2);
+    e1->addAnimal(a3);
+
+ 
+    Visitor* v = new Visitor("Sarah Ali", 3);
+
+    
+    e1->displayAnimals(1);
+    cout << "Visitor Info:\n";
+    cout << "Name: " << v->getVisitorName() << "\n";
+    cout << "Tickets Bought: " << v->getTicketsBought() << "\n";
+
+   
+    delete v;   
+    delete e1;  
+
     return 0;
 }
